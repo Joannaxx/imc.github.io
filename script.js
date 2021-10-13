14e460b32a41013055332c8957e817109155171d
Joannaxx <85002893+Joannaxx@users.noreply.github.com>
2021-10-12 21:04:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-function calculo_IMC(){
-    var nome, peso, altura, calc, tipo;
-    nome = window.prompt("Digite seu nome");
-    peso = window.prompt("Digite seu peso");
-    peso = parseFloat(peso);
-    altura = window.prompt("Digite sua altura (em centimetros)");
-    altura = parseFloat(altura);
-    calc = peso/(altura**2);
-    document.write("Seu IMC é: ",calc);
-    if (calc <16,9) {
-        tipo="muito abaixo do peso";
-    } 
+const  calculo  =  document.getElementById('calculo');
 
-    if(calc <=18,4){
-        tipo="abaixo do peso"
+function IMC(){
+    const  nome  =  document.getElementById('nome').value;
+    const  altura  =  document.getElementById('altura').value;
+    const peso =  document.getElementById('peso').value;
+    const  resultado  =  document.getElementById('resultado');
+    
+    if(nome !== '' && altura !== '' && peso!== ''){
+        const calc = (peso/(altura**2)).toFixed(1);
+        let tipo = '';
+        if (calc <=16.9) {
+            tipo="muito abaixo do peso.";
+        }else
+
+        if(calc <=18.4){
+            tipo="abaixo do peso."
+        }else
+
+        if(calc <=24.9){
+            tipo="com o peso ideal."
+        }else
+
+        if(calc <=29.9){
+            tipo="acima do peso."
+        }else
+
+        if(calc <=34.9){
+            tipo="com obesidade grau I."
+        }else
+
+        if(calc <=40){
+            tipo="com obesidade grau II."
+        }else{
+            tipo="com obesidade grau III- mórbida."
+        }
+        
+        resultado.textContent = `${nome}  seu IMC é de ${calc} e você está ${tipo}`;
+        
+        
     }
-
-    if(calc <=24,9){
-        tipo="peso ideal"
-    }
-
-    if(calc <=29,9){
-        tipo="acima do peso"
-    }
-    if(calc <=34,9){
-        tipo="obesidade grau I"
-    } 
-
-    if(calc <=40){
-        tipo="obesidade grau II"
-    }
-
-    if(calc >40){
-        tipo="obesidade grau III- mórbida"
-    }
-
-
-
-    document.write(" ",nome," seu IMC é ", calc, " e você está ", tipo);
-
+    console.log(nome);
+    console.log(altura);
+    console.log(peso);
+    console.log(resultado);
+    console.log(calculo);
 
 }
+calculo.addEventListener('click',IMC);
